723dc83489bb6a647594f0475a20075c93e55f75
human2402 <oleg.reymon24@outlook.com>
2020-08-01 17:05:01 +0900

    majir patch

---
+++ build/precache-manifest.55eb18642b77fd708a90f80a5214c951.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d6010473ea316f9d858df0d59ef238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dafeb2c474175357ea3a",
+    "url": "/static/css/main.cdbd8f4a.chunk.css"
+  },
+  {
+    "revision": "4961614d005d7075ae35",
+    "url": "/static/js/2.0b59ff75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b59ff75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafeb2c474175357ea3a",
+    "url": "/static/js/main.91e4a3e1.chunk.js"
+  },
+  {
+    "revision": "bd9fac0adaa8dd45302f",
+    "url": "/static/js/runtime-main.aae324a9.js"
+  },
+  {
+    "revision": "77daf1c004bb2ead5fc88c811186c917",
+    "url": "/static/media/history.77daf1c0.svg"
+  },
+  {
+    "revision": "cac7e8d404cbc844a90e139658e30988",
+    "url": "/static/media/history.cac7e8d4.svg"
+  },
+  {
+    "revision": "29677998adf5e34f177d3b2237e75353",
+    "url": "/static/media/insta.29677998.svg"
+  },
+  {
+    "revision": "c5e3f21fb5f4db5d36b9f6e1605f6008",
+    "url": "/static/media/settings.c5e3f21f.svg"
+  },
+  {
+    "revision": "cbd866a1652eefc6e7df7ee220306c4b",
+    "url": "/static/media/settings.cbd866a1.svg"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CalculatorAppTwo from './CalculatorApp2'
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+    <CalculatorAppTwo />
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
